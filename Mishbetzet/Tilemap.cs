7ee2682bb7616ec5a821a681e526249ea23205d1
EladReuven <102731503+EladReuven@users.noreply.github.com>
2022-12-30 18:06:24 +0200

    Update Tilemap.cs

--- Mishbetzet/Tilemap.cs
+++ Mishbetzet/Tilemap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace Mishbetzet
 {
-    internal class Tilemap
+    internal class Tilemap : IEnumerable<int>
     {
         Tile[,] _tiles;
+        Dictionary<Tile, int> index; //implementation for int index?
+                                     //every tile has an int key in the tilemap instead of on the tile itself
 
         public int Width { get => _tiles.GetLength(0); }
         public int Height { get => _tiles.GetLength(1); }
@@ -16,6 +19,16 @@ namespace Mishbetzet
         public Tilemap(int width, int height)
         {
             _tiles = new Tile[width, height];
+
+            int count = 0;
+            for (int i = 0; i < width; i++;)
+            {
+                for (int j = 0; j < height; j++;)
+                {
+                    index.Add(new Point(j, i), count);
+                    count++;
+                }
+            }
         }
 
         public Tile GetTile(int x, int y)
@@ -23,5 +36,25 @@ namespace Mishbetzet
             //TODO check if x and y are in bounds
             return _tiles[x, y];
         }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int = _tiles.Length)
+            {
+
+            }
+            //foreach(Tile tile in _tiles)
+            //{
+            //    yield return tile;
+            //}
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    for (int i = _tiles.Length; i >= 0; i--;)
+        //    {
+                
+        //    }
+        //}
     }
 }
