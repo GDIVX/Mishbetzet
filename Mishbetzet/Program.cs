0df9abfacbc08d560dd899ebb29c5de68c83b28b
GDIVX <yarin107@gmail.com>
2023-02-06 18:50:21 +0200

    Update Program.cs

--- Mishbetzet/Program.cs
+++ Mishbetzet/Program.cs
@@ -13,6 +13,36 @@ class Program
     static void Main(string[] args)
     {
         Core.Main.CreateTileMap(10, 10);
-        Core.Main.Run();
+
+
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                Core.Main.CreateTile<BaseTile>(new(x, y));
+            }
+        }
+
+        foreach (var tile in Core.Main.Tilemap)
+        {
+            Console.WriteLine(tile);
+            Core.Main.CreateGameObject<BaseGameObject>(tile);
+        }
+        
+    }
+}
+
+public class BaseGameObject : GameObject
+{
+    public override void Step(Point direction)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class BaseTile : Tile
+{
+    public BaseTile(Point position) : base(position)
+    {
     }
 }
